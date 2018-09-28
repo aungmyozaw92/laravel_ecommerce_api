8b0b2d407c13aa906dd10590273bd122b407e9b2
aungmyozaw <aungmyo.zaw@code2lab.com>
2018-09-28 16:48:44 +0630

    creae and updae api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\ProductRequest;
+
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -36,9 +45,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+        return response(['data' => new ProductResource($product)],Response::HTTP_CREATED);
     }
 
     /**
@@ -63,7 +81,13 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail'] = $request->description;
+
+        unset($request['description']);
+        $product->update($request->all());
+
+        return response(['data' => new ProductResource($product)],Response::HTTP_CREATED);
+        
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,8 +6,12 @@ use App\Model\Review;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Product extends Model  
 {
+	protected $fillable = [
+		'name', 'detail', 'stock', 'price', 'discount'
+	];
+
     public function reviews()
     {
     	return $this->hasMany(Review::class);
