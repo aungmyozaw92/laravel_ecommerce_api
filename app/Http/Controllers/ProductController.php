f821dfbfe428d6c2af0bb14d8e1c6dd9a4337796
aungmyozaw <aungmyo.zaw@code2lab.com>
2018-09-30 21:09:35 +0630

    delete method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -87,7 +87,7 @@ class ProductController extends Controller
         $product->update($request->all());
 
         return response(['data' => new ProductResource($product)],Response::HTTP_CREATED);
-        
+
     }
 
     /**
@@ -98,6 +98,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
